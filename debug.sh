a2fb67abf4d7b2d7fe33a6383895f3fc7fe96efb
Timo Tabertshofer <timo.tabertshofer@gmail.com>
2024-01-21 18:43:41 +0100

    Update syntax in scripts

--- debug.sh
+++ debug.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-docker-compose -f docker-compose.local.yaml down
+docker compose -f docker-compose.local.yaml down
 
 ./microservices/api-gateway/gradlew -p ./microservices/api-gateway/ clean assemble
 ./microservices/authorization-server/gradlew -p ./microservices/authorization-server/ clean assemble
 ./microservices/user-service/gradlew -p ./microservices/user-service/ clean assemble
 
-docker-compose -f docker-compose.local.yaml up --build
-
+docker compose -f docker-compose.local.yaml up --build
