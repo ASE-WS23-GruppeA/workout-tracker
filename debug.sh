7e41ae762f6176935daa1fc1c21b7750fa75ac56
Timo Tabertshofer <timo.tabertshofer@gmail.com>
2024-01-24 22:25:30 +0100

    Update run scripts

--- debug.sh
+++ debug.sh
@@ -5,6 +5,7 @@ docker compose -f docker-compose.local.yaml down
 ./microservices/api-gateway/gradlew -p ./microservices/api-gateway/ clean assemble
 ./microservices/authorization-server/gradlew -p ./microservices/authorization-server/ clean assemble
 ./microservices/user-service/gradlew -p ./microservices/user-service/ clean assemble
+./microservices/analytics-service/gradlew -p ./microservices/user-service/ clean assemble
 
 (
   cd ./microservices/frontend/FE_Fitness_Tracker || exit
